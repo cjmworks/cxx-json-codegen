454235c96c4a88ac827c84e7cc12652ffac826e4
lmingzhi618 <lmingzhi618@gmail.com>
2026-09-23 21:47:50 -0400

    test(simdjson): preserve nested encode error paths

--- tests/backends/simdjson/nested_encode_generated_tests.cpp
+++ tests/backends/simdjson/nested_encode_generated_tests.cpp
@@ -22,3 +22,20 @@ TEST_CASE("object.round_trip", "[simdjson][encoder][decoder]") {
     REQUIRE(decode_error.code == cjm::simdjson::DecodeErrorCode::none);
     REQUIRE(decode_error.runtime_error == ::simdjson::SUCCESS);
 }
+
+TEST_CASE("object.error_path", "[simdjson][encoder]") {
+    app::User value;
+    value.address.city = std::string{"\xC3\x28", 2};
+    cjm::simdjson::EncodeError error;
+
+    const auto result = cjm::simdjson::to_json(value, error);
+
+    REQUIRE_FALSE(result.has_value());
+    REQUIRE(error.code == cjm::simdjson::EncodeErrorCode::invalid_utf8_string);
+    REQUIRE(error.runtime_error == ::simdjson::UTF8_ERROR);
+    REQUIRE(error.path.size() == 2);
+    REQUIRE(error.path[0].kind == cjm::simdjson::EncodePathSegmentKind::field);
+    REQUIRE(error.path[0].field_name == "home");
+    REQUIRE(error.path[1].kind == cjm::simdjson::EncodePathSegmentKind::field);
+    REQUIRE(error.path[1].field_name == "city");
+}
